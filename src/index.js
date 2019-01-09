47646142117a1a2976fed304603e24a7c6667548
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2019-01-09 00:28:24 -0500

    CommentDetail.js added

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
       <div className="ui container comments">
-          <CommentDetail/>
-          <CommentDetail/>
-          <CommentDetail/>
-          <CommentDetail/>
-          <CommentDetail/>
+          <CommentDetail author="Sam"/>
+          <CommentDetail author="Alex"/>
+          <CommentDetail author="Jane"/>
       </div>
     );
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
+
